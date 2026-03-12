a5ec263d0afc6e8523bd38d6abfa02f15be1552f
ghiyas <ghiyasnafitsanierawan@gmail.com>
2026-03-12 14:23:37 +0700

    commit pertama

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char ** argv) {
+
+    string a ;
+    cout <<"input nama anda: ";
+    cin >> a ;
+    cout << "Hello " << a  << endl;
+
+    return 0;
+}
